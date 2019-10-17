a1290fd5d76121c528a38968352031e51e6384a7
sucre <sucre.zhang@rakwireless.com>
2019-10-17 09:36:15 +0800

    Delete useless code.

--- based on RAK4600/4600/at.h
+++ based on RAK4600/4600/at.h
@@ -19,27 +19,6 @@
 #define RAK_ERROR_LORA		    0x10
 #define RAK_ERROR_NOT_JOIN      0x11
 
-#ifdef BG96_TEST
-#define AT_HELP		\
-"at+version----Get the current firmware version number\n \
-at+set_config=device:restart----device restart cmd\n \
-at+get_config=device:status----show all components status\n \
-at+set_config=device:sleep:X----device sleep command\n \
-at+set_config=device:gps:X----gps on/off command\n \
-at+set_config=device:cellular:X----cellular on/off command\n \
-at+set_config=cellular:send_interval:X:Y----device period send task\n \
-at+scan=cellular----cellular search net command\n \
-at+set_config=cellular:XXX:Y:ZZZ: AAA:BBB:C----cellular config join parameters\n \
-at+set_config=cellular:(XXX)----cellular AT unvarnished transmission\n \
-at+send=cellular:XXX----cellular send manually\n \
-at+help----show all at command supported\n \
-at+set_config=hologram:----set hologram card id\n \
-at+send=hologram:user:----send user define data\n \
-at+send=hologram:sensor----send device data\n \
-at+set_config=ble:work_mode:----ble mode choose\n"
-#endif
-
-#ifdef LORA_4600_TEST
 #define AT_HELP     \
 "at+version----Get the current firmware version number \n\
 at+set_config=device:restart----device restart cmd \n\
@@ -65,11 +44,6 @@ at+set_config=lora:dr:X----lora data rate \n\
 at+set_config=lora:send_interval:X:Y----lora send interval \n\
 at+get_config=lora:status----lora status \n\
 at+get_config=lora:channel----lora channel \n"
-#endif
 
-#ifndef AT_HELP
-#define AT_HELP " "
-#endif
-void usbd_send(uint8_t *pdata, uint16_t len);
 #endif
 
