7aea9d5b86db336f013111307f7d6fce75791dd9
sucre <sucre.zhang@rakwireless.com>
2019-10-17 19:11:45 +0800

    Change callback register api in user_app.c.

--- based on RAK4600/4600/user_app.c
+++ based on RAK4600/4600/user_app.c
@@ -19,10 +19,10 @@ void user_sensor_wakeup(void)
 void main(void)
 {
     //system init 
+    rui_sensor_register_callback(user_sensor_wakeup, user_sensor_sleep);
     rui_init();
     
     //you can add your init code here, like timer, uart, spi...
-    rui_sensor_register_callback(user_sensor_wakeup, user_sensor_sleep);
 
     while(1)
     {
