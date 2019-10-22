d58243481b671da52861eedddd68088a8de9f7cc
nero.du <nero.du@rakwireless.com>
2019-10-22 11:39:33 +0800

    app

--- based on RAK5010/5010+W25Q32/user_app.c
+++ based on RAK5010/5010+W25Q32/user_app.c
@@ -33,16 +33,48 @@ void sensor_off(void)
     lps22hb_mode(0);
 }
 
+
+/*This demo shows how to connect to external spi flash on 5010.
+W25Q32 4M flash with spi. Pin map is below and VREF on 5010 must
+be powered by external 3.3V.
+
+   W25Q32       5010
+   GND --------- GND
+   VCC --------- VBAT
+   CS  --------- IO4 P1.01  //33
+   DO  --------- IO3 P1.02  //34
+   DI  --------- IO2 P0.20
+   CLK --------- IO1 P0.19
+*/
+RUI_SPI_ST w_st = {0};
+
+void spi_init()
+{
+    w_st.PIN_CS = 33;
+    w_st.PIN_MISO = 34;
+    w_st.PIN_MOSI = 20;
+    w_st.PIN_SCL = 19;
+    rui_spi_init(&w_st);
+}
+
+void W25Q32_get_id()
+{
+    uint8_t cmd[6] = {0x90,0x00,0x00,0x00,0xFF,0xFF};
+    uint8_t rsp[6] = {0};
+    rui_spi_rw(RUI_IF_READ,cmd,6,rsp,6);
+    NRF_LOG_INFO("%X%X",rsp[4],rsp[5]);
+}
+
 void main(void)
 {
     //system init 
     rui_sensor_register_callback(sensor_on,sensor_off);
     rui_init();
-
+    spi_init();
     while(1)
     {
         //do your work here, then call rui_device_sleep(1) to sleep
-
+        W25Q32_get_id();
         //here run system work and do not modify
         rui_running();
     }
